1273d4e02e16757f96e17baa9b96014dae7c1509
Michael New <mikenew12@gmail.com>
2022-01-08 13:02:16 -0800

    Add a simulation for the host intentionally choosing a non-prize door

--- monty_hall.nim
+++ monty_hall.nim
@@ -2,22 +2,56 @@ import std/random
 
 randomize()
 
-let trials = 100000
 
+# Run the simulation assuming the host has no knowledge of where the prize is
+var trials = 100000
 var original_door_correct_count = 0
 var remaining_door_correct_count = 0
 
 while original_door_correct_count + remaining_door_correct_count < trials:
-  let prize_door = rand(1..3)
-  let contestant_choice = rand(1..3)
-  let host_choice = rand(1..3)
+  var doors = @[0,1,2]
+  let prize_door = rand(0..2)
+  let contestant_choice = rand(0..2)
+
+  doors.delete(contestant_choice)
+  let host_choice = doors[rand(0..1)]
 
   # The only valid game state is if the host randomly chooses the non-prize door
-  # If they choose the contestants door or the prize door, re-run the simulation
+  # If the host chooses the door with the prize behind it, discard this game and run again
+  if host_choice != prize_door:
+    if contestant_choice == prize_door: original_door_correct_count += 1
+    else: remaining_door_correct_count += 1
+
+echo "Running the simulation " & $trials & " times where the host chooses doors randomly"
+echo $original_door_correct_count & " # of times the contestan'ts originally chosen door contained the prize"
+echo $remaining_door_correct_count & " # of times the remaining door contained the prize"
+
+
+
+# This time simulate the host knowingly choosing a door without the prize
+trials = 100000
+original_door_correct_count = 0
+remaining_door_correct_count = 0
+
+while original_door_correct_count + remaining_door_correct_count < trials:
+  let prize_door = rand(0..2)
+  let contestant_choice = rand(0..2)
+  var host_choice = -1
+
+  var doors = @[0,1,2]
+
+  if contestant_choice != prize_door:
+    doors.delete(contestant_choice)
+    doors.delete(prize_door)
+    host_choice = doors[0]
+  else:
+    doors.delete(contestant_choice)
+    host_choice = doors[rand(0..1)]
+
   if host_choice != contestant_choice and host_choice != prize_door:
     if contestant_choice == prize_door: original_door_correct_count += 1
     else: remaining_door_correct_count += 1
 
-echo "Running the simulation " & $trials & " times"
+echo "\n\nRunning the simulation " & $trials & " times where the host intentionally chooses empty doors"
 echo $original_door_correct_count & " # of times the contestan'ts originally chosen door contained the prize"
 echo $remaining_door_correct_count & " # of times the remaining door contained the prize"
